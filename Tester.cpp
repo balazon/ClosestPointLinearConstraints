ca4d77cde15b7b572a62c8cf00ddc0bc8dc559de
balazon <balazon@gmail.com>
2015-07-17 12:52:49 +0200

    5 tests written

--- Tester.cpp
+++ Tester.cpp
@@ -30,9 +30,29 @@ void Tester::InitTests()
 				  1.f, -1.f, 0,
 				  0, 1.f, 0};
 	constraints.assign(c1, c1 + 3 * 3);
-	Test t{1.f, 1.f, constraints, false, 0.f, 0.f};
-	AddTest(t);
+	Test t1{1.f, 1.f, constraints, false, 0.f, 0.f};
+	AddTest(t1);
 	
+	float c2[] = {-1.f, 1.f, 2.5f,
+				  0.f, 1.f, 5.f,
+				  1.f, 0.f, 5.f};
+	constraints.assign(c2, c2 + 3 * 3);
+	Test t2{2.5f, 2.5f, constraints, true, 2.5f, 2.5f};
+	AddTest(t2);
+	
+	Test t3{7.f, -2.f, constraints, true, 7.f, 5.f};
+	AddTest(t3);
+	
+	Test t4{7.f, 7.f, constraints, true, 5.f, 5.f};
+	AddTest(t4);
+	
+	float c5[] = {1.f, 1.f, 7.f,
+				  -2.f, 1.f, 4.f,
+				  -2.f, -1.f, 4.f,
+				  0.666667f - 1.f, 1.33333f};
+	constraints.assign(c5, c5 + 3 * 4);
+	Test t5{-3.f, 3.f, constraints, true, -1.f, 2.f};
+	AddTest(t5);
 }
 
 void Tester::AddTest(Test t)
